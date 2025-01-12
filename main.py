53fb82dfc99804bd1f1e1da300cac7b898ff37a4
José Sixpenze <developedbyjms@gmail.com>
2025-01-12 23:05:02 +0200

    main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from dnn import DeepNeuralNetwork
 
 xor = [[[0, 0], [0]], [[0, 1], [1]],[[1, 0], [1]],[[1, 1], [0]]]
 
-dnn = DeepNeuralNetwork(dataset = admission_data(), hidden_layers = [20, 10, 20])
-dnn.train(learning_rate=0.10, epoch=10000, randomly=True)
+dnn = DeepNeuralNetwork(dataset = xor, hidden_layers = [4, 2, 4])
+dnn.train(learning_rate=0.10, epoch=1000, randomly=True)
 print(dnn.feedforward([-1, -1]))
 
 
